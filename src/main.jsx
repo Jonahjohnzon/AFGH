c51d42f3df8d07b09da68c8b5f7705f9fbdaa1f0
loozebox <loozebox@Loozebox.Loozebox>
2023-10-21 21:34:46 +0100

    compressed files

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,11 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
+if (process.env.NODE_ENV === 'production'){
+  disableReactDevTools()
+}
 const query = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
